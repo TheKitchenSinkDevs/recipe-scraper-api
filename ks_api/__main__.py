fee6b54b8f44893868413f59e1aa7676e1c86acf
Eric Udlis <udlis.eric@gmail.com>
2021-07-16 19:10:05 -0500

    Wrote POST feature

---
+++ ks_api/__main__.py
@@ -0,0 +1,65 @@
+import string
+
+from fastapi import FastAPI
+from recipe_scrapers import scrape_me
+from base64 import b64encode, b64decode
+import logging
+
+app = FastAPI()
+
+log = logging.getLogger("RECIPIE_SCRAPER")
+log.setLevel(logging.INFO)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+
+@app.post("/parse/{url}")
+async def parse(url: str):
+    url = b64decode(url).decode('utf-8')
+    log.info(url)
+    try:
+        scraped = scrape_me(url)
+        ingredients = parse_ingredients(scraped.ingredients())
+        failed = False
+    except BaseException as err:
+        log.error(err)
+        failed = True
+
+    if failed:
+        return {"message": "Failed to scrape url"}
+    else:
+        recipe = {
+                "Name": scraped.title(),
+                "url": scraped.url,
+                "image": scraped.image(),
+                "yields": scraped.yields(),
+                "ingredients": ingredients,
+                "instructions": scraped.instructions().split("\n")
+                }
+        return {"message": "Site Scraped!",
+                "recipe": recipe}
+
+def parse_ingredients(ingredients: "list[str]"):
+    output = []
+    for line in ingredients:
+        words = line.split(" ")
+        for idx, word in enumerate(words):
+            if word in [digit for digit in string.digits]:
+                number = int(word)
+                measurement = words[idx + 1]
+                if number > 1 and measurement[-1] == 's':
+                    measurement = measurement[:-1]
+                food = " ".join(words[idx + 2:])
+                output.append({
+                        "food": food,
+                        "quantity": number,
+                        "measurement": measurement
+                        })
+                break
+
+    return output
+
+
+
